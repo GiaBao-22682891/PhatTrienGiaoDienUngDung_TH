558a5e402242dfbaa050675579a464d4d2b6197f
Bao To <you@example.com>
2025-01-08 23:50:47 +0700

    Challenge 4

--- script.js
+++ script.js
@@ -36,3 +36,10 @@ if (avgD > avgK && avgD >= 100) {
     console.log(`Koalas win the trophy with score ${avgK}>${avgD}`)
 } else
     console.log('No one wins the trophy!')
+
+// Coding Challenge #4
+
+var bill = 275
+var tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
