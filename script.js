c704e65bd580a59a8361461f8dd940cf92240269
Bao To <you@example.com>
2025-01-08 23:41:32 +0700

    Challenge 1

---
+++ script.js
@@ -0,0 +1,17 @@
+// Coding Challenge #1
+var mark_weight = 78
+var mark_height = 1.69
+
+var john_weight = 95
+var john_height = 1.88
+
+var BMIM = mark_weight / mark_height ** 2
+var BMIJ = john_weight / john_height ** 2
+
+var higherBMI = BMIM > BMIJ
+
+console.log(BMIM, BMIJ, higherBMI)
+
+
+
+
